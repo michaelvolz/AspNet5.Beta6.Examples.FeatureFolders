6fe124be37e3b8552ee94d16b16c62864c9445db
Michael A. Volz <mvolz@redmuffin.de>
2015-08-15 11:40:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# AspNet5.Beta6.Examples.FeatureFolders
+Asp.Net 5 Beta 6 - Feature Folders using IViewLocationExpander
+
+A simple implementation of Feature Folders for Asp.Net MVC inspired by this blog post: http://timgthomas.com/2013/10/feature-folders-in-asp-net-mvc/
+
+This is the original Asp.Net 5 Beta 6 MVC 6 project just restructured and with a modified RazorViewEngine using IViewLocationExpander.
---
+++ src/Web/App/FeatureFolderLocationRemapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc.Razor;
+
+namespace AspNet5.Beta6.Examples.FeatureFolders.App
+{
+    /// <summary>
+    ///     Relocates the view folder mapping to "/App/Features/" and renames the "Shared" folder to "_Shared"
+    /// </summary>
+    /// <remarks>
+    ///     Based on this blog post by Jeff Fritz:
+    ///     http://www.jeffreyfritz.com/2015/05/customize-asp-net-mvc-6-view-location-easily/
+    ///     MVC Feature Folders explained: http://timgthomas.com/2013/10/feature-folders-in-asp-net-mvc/
+    ///     Add this code snippet to your services configuration in StartUp.cs:
+    ///     <c>
+    ///         services.Configure&lt;RazorViewEngineOptions>(o => 
+    ///         {
+    ///         o.ViewLocationExpanders.Add(new FeatureFolderLocationRemapper());
+    ///         });
+    ///     </c>
+    /// </remarks>
+    public class FeatureFolderLocationRemapper : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context,
+            IEnumerable<string> viewLocations)
+        {
+            return viewLocations.MoveViewsIntoFeaturesFolder().CutomizeSharedWithUnderScore();
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            // do nothing.. not entirely needed for this 
+        }
+    }
+
+    public static class FeatureFolderExtensions
+    {
+        public static IEnumerable<string> MoveViewsIntoFeaturesFolder(this IEnumerable<string> viewLocations)
+        {
+            // I added an additional "App" folder in my MVC project to separate the root configuration 
+            // and package files from my application code. 
+            return viewLocations.Select(f => f.Replace("/Views/", "/App/Features/"));
+        }
+
+        public static IEnumerable<string> CutomizeSharedWithUnderScore(this IEnumerable<string> viewLocations)
+        {
+            return viewLocations.Select(f => f.Replace("/Shared/", "/_Shared/"));
+        }
+    }
+}
